0fdd81b10d74bc2d714d306194f72253aa867c38
Corlene M. Ankrum <C.ANKRUM@labs.clark.edu>
2019-02-26 10:39:43 -0800

    added search

--- search-records.php
+++ search-records.php
@@ -17,7 +17,7 @@ require 'inc/app/config.inc.php';
             if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 // If there is content in the search field, see if that content is in the db
                 if(!empty($_POST['search'])){
-                    $sql = "SELECT * FROM $db_table WHERE " . '"' . $_POST["search"] . '"' . " IN (student_id, first_name, last_name, email, phone) ORDER BY last_name ASC";
+                    $sql = "SELECT * FROM $db_table WHERE " . '"' . $_POST["search"] . '"' . " IN (student_id, first_name, last_name, email, phone, financial_aid, degree_program) ORDER BY last_name ASC";
                     // $sql = "SELECT * FROM student WHERE student_id LIKE '%val%' or field2 LIKE '%val%'
                     $result = $db->query($sql);
 // IF there are no matching results, display message that nothing matches, display sad face
