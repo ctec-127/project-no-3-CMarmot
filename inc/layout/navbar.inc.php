77e70e3e2925d757aeca20ab40394c3596e41d12
CMarmot <corleneankrum@gmail.com>
2019-03-13 13:07:18 -0700

    Added item to navbar

--- inc/layout/navbar.inc.php
+++ inc/layout/navbar.inc.php
@@ -12,6 +12,9 @@
       </li>
       <li class="nav-item <?= echoActiveClassIfRequestMatches("create-record");?>"">
         <a class="nav-link" href="create-record.php">Create Record</a>
+      </li>    
+      <li class="nav-item <?= echoActiveClassIfRequestMatches("advanced-search1");?>"">
+        <a class="nav-link" href="advanced-search1.php">Advanced Search</a>
       </li>
 
       <li class="nav-item dropdown">
